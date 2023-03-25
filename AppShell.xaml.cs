05edcab03cd048afa6d5c9eb84fc618b9d0824b6
yatha <yatha@YATHARTHSPC>
2023-03-25 17:08:04 -0400

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ForecastApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ForecastApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
