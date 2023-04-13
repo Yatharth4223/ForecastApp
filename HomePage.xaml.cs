0418c2b5cb18df06eb12c990ad77202428cd5357
Tauheed Attar <attart@sheridancollege.ca>
2023-04-13 16:58:27 -0400

    Changed MatchSelectionPage so that LaLiga and IPl as a common variable to pass

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -4,8 +4,8 @@ namespace ForecastApp;
 
 public partial class HomePage : ContentPage
 {
-    
 
+    
     public HomePage()
 	{
 		InitializeComponent();
@@ -13,18 +13,19 @@ public partial class HomePage : ContentPage
 
     async private void SettingsButton_Clicked(object sender, EventArgs e)
     {
+        
         await Navigation.PushAsync(new Setting());
     }
 
     async private void LaLigaMatchSelectionButton_Clicked(object sender, EventArgs e)
     {
-        
-        await Navigation.PushAsync(new MatchSelection(false));
+        bool isIpl = false;
+        await Navigation.PushAsync(new MatchSelection(isIpl)) ;
     }
 
     private async void IplMatchSelectionButton_Clicked(object sender, EventArgs e)
     {
-        
-        await Navigation.PushAsync(new MatchSelection(true));
+        bool isIpl = true;
+        await Navigation.PushAsync(new MatchSelection(isIpl));
     }
 }
--- MatchSelection.xaml.cs
+++ MatchSelection.xaml.cs
@@ -9,24 +9,36 @@ public partial class MatchSelection : ContentPage
 	{
 		InitializeComponent();
         _isIpl = isIPL;
-        if (_isIpl) 
-        { 
+        if (_isIpl == true)
+        {
             TeamsListView.ItemsSource = IplRepository.GetTeams();
         }
-        TeamsListView.ItemsSource = TeamRepository.GetTeams();
+        else
+        {
+            TeamsListView.ItemsSource = TeamRepository.GetTeams();
+
+        }
 
     }
 
     private void SearchName_TextChanged(object sender, TextChangedEventArgs e)
     {
-        if(_isIpl)
+        if (_isIpl)
+        {
             TeamsListView.ItemsSource = IplRepository.SelectByName(SearchName.Text);
-        TeamsListView.ItemsSource = TeamRepository.SelectByName(SearchName.Text);
+
+        }
+        else
+        {
+            TeamsListView.ItemsSource = TeamRepository.SelectByName(SearchName.Text);
+
+        }
     }
 
     private void TeamsListView_ItemTapped(object sender, ItemTappedEventArgs e)
     {
         var selectedMatchVaribale = TeamsListView.SelectedItem as Team;
+        Navigation.PushAsync(new StatsPage(selectedMatchVaribale));
         
     }
 }
