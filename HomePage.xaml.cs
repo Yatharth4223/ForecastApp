fc6172bc9a0963ab6ceed2b8da64bdc8a1d57124
Tauheed Attar <attart@sheridancollege.ca>
2023-04-18 14:32:45 -0400

    Updated Dark mode in SettingPage

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -1,31 +1,51 @@
 
 
+
+
+using ForecastApp.Themes;
+
 namespace ForecastApp;
 
 public partial class HomePage : ContentPage
 {
 
-    
-    public HomePage()
+    private bool _darkmode;
+    public HomePage(bool darkMode)
 	{
 		InitializeComponent();
-	}
+        _darkmode = darkMode;
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (darkMode == true)
+        {
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new DarkTheme());
+            
+
+        }
+        else
+        {
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new LightTheme());
+        }
+
+    }
 
     async private void SettingsButton_Clicked(object sender, EventArgs e)
     {
         
-        await Navigation.PushAsync(new Setting());
+
+        await Navigation.PushAsync(new Setting(_darkmode));
     }
 
     async private void LaLigaMatchSelectionButton_Clicked(object sender, EventArgs e)
     {
         bool isIpl = false;
-        await Navigation.PushAsync(new MatchSelection(isIpl)) ;
+        await Navigation.PushAsync(new MatchSelection(isIpl,_darkmode)) ;
     }
 
     private async void IplMatchSelectionButton_Clicked(object sender, EventArgs e)
     {
         bool isIpl = true;
-        await Navigation.PushAsync(new MatchSelection(isIpl));
+        await Navigation.PushAsync(new MatchSelection(isIpl,_darkmode));
     }
 }
--- MatchSelection.xaml.cs
+++ MatchSelection.xaml.cs
@@ -1,11 +1,16 @@
 using ForecastApp.Models;
+using ForecastApp.Themes;
 
 namespace ForecastApp;
 
 public partial class MatchSelection : ContentPage
 {
     private bool _isIpl;
-	public MatchSelection(bool isIPL)
+
+    private bool _darkMode;
+
+    public static bool isToggled = false;
+	public MatchSelection(bool isIPL,bool darkMode)
 	{
 		InitializeComponent();
         _isIpl = isIPL;
@@ -18,6 +23,21 @@ public partial class MatchSelection : ContentPage
             TeamsListView.ItemsSource = TeamRepository.GetTeams();
 
         }
+        _darkMode = darkMode;
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (_darkMode == true)
+        {
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new DarkTheme());
+
+
+        }
+        else
+        {
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new LightTheme());
+        }
+
 
     }
 
