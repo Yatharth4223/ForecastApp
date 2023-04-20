dcea272d2f092ff8211de4bbef157ffc3104001f
Tauheed Attar <attart@sheridancollege.ca>
2023-04-19 21:04:21 -0400

    Removed error

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -1,3 +1,5 @@
+
+
 using ForecastApp.Themes;
 
 namespace ForecastApp;
@@ -5,15 +7,12 @@ namespace ForecastApp;
 public partial class HomePage : ContentPage
 {
 
-<<<<<<< HEAD
+
     
-    public HomePage()
-=======
+
     private bool _darkmode;
     public HomePage(bool darkMode)
-<<<<<<< HEAD
->>>>>>> 6f8e67b02661a6bff72fb5976d6747861ed3fae2
->>>>>>> b216b6bba9287f17a737968e1feeeccce37fc429
+
 	{
 		InitializeComponent();
         
@@ -22,87 +21,30 @@ public partial class HomePage : ContentPage
     }
 
     async private void SettingsButton_Clicked(object sender, EventArgs e)
-<<<<<<< HEAD
+
     {   
         await Navigation.PushAsync(new Setting(_darkMode));
-=======
-    {
-        
 
-<<<<<<< HEAD
-        await Navigation.PushAsync(new Setting());
-=======
-        await Navigation.PushAsync(new Setting(_darkmode));
->>>>>>> 6f8e67b02661a6bff72fb5976d6747861ed3fae2
->>>>>>> b216b6bba9287f17a737968e1feeeccce37fc429
-=======
-	{
-		InitializeComponent();
-        _darkmode = darkMode;
-        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
-        if (darkMode == true)
-        {
-            mergedDictionaries.Clear();
-            mergedDictionaries.Add(new DarkTheme());     
-        }
-        else
-        {
-            mergedDictionaries.Clear();
-            mergedDictionaries.Add(new LightTheme());
-        }
-    }
-
-    async private void SettingsButton_Clicked(object sender, EventArgs e)
-    {           
-        await Navigation.PushAsync(new Setting(_darkmode));
->>>>>>> 86d14843e8071610416eb3fe340684ee1abe4921
     }
 
     async private void LaLigaMatchSelectionButton_Clicked(object sender, EventArgs e)
     {
-<<<<<<< HEAD
         bool isIpl = false;
-<<<<<<< HEAD
-        await Navigation.PushAsync(new MatchSelection(isIpl)) ;
-=======
-<<<<<<< HEAD
-        await Navigation.PushAsync(new MatchSelection(isIpl, _darkMode)) ;
-=======
-<<<<<<< HEAD
+
         await Navigation.PushAsync(new MatchSelection(isIpl,_darkmode)) ;
-=======
-        await Navigation.PushAsync(new MatchSelection(isIpl)) ;
->>>>>>> 0418c2b5cb18df06eb12c990ad77202428cd5357
->>>>>>> 746d8c12b416a6b117d49a30d22eda31b8357fe1
->>>>>>> 6f8e67b02661a6bff72fb5976d6747861ed3fae2
->>>>>>> b216b6bba9287f17a737968e1feeeccce37fc429
-=======
-            bool isIpl = false;
-            await Navigation.PushAsync(new MatchSelection(isIpl, _darkmode));
->>>>>>> 86d14843e8071610416eb3fe340684ee1abe4921
+
+        
+
     }
 
     private async void IplMatchSelectionButton_Clicked(object sender, EventArgs e)
     {
         bool isIpl = true;
-<<<<<<< HEAD
-<<<<<<< HEAD
-        await Navigation.PushAsync(new MatchSelection(isIpl));
-=======
-<<<<<<< HEAD
-        await Navigation.PushAsync(new MatchSelection(isIpl, _darkMode));
 
-=======
-<<<<<<< HEAD
+
         await Navigation.PushAsync(new MatchSelection(isIpl,_darkmode));
-=======
-        await Navigation.PushAsync(new MatchSelection(isIpl));
->>>>>>> 0418c2b5cb18df06eb12c990ad77202428cd5357
->>>>>>> 746d8c12b416a6b117d49a30d22eda31b8357fe1
->>>>>>> 6f8e67b02661a6bff72fb5976d6747861ed3fae2
->>>>>>> b216b6bba9287f17a737968e1feeeccce37fc429
-=======
-        await Navigation.PushAsync(new MatchSelection(isIpl,_darkmode));
->>>>>>> 86d14843e8071610416eb3fe340684ee1abe4921
+
+        
+
     }
 }
--- Setting.xaml.cs
+++ Setting.xaml.cs
@@ -1,14 +1,21 @@
+
 using ForecastApp.Models;
 using ForecastApp.Themes;
+
 
 namespace ForecastApp;
 
 public partial class Setting : ContentPage
 {
+
+	
+
 	private List<Team> _football;
 	private List<Team> _cricket;
 	bool IsDarkMode;
+
 	public Setting(bool isDarkMode)
+
 	{
 		InitializeComponent();
         var darkMode = isDarkMode ? true : false;
@@ -18,18 +25,28 @@ public partial class Setting : ContentPage
             mergedDictionaries.Clear();
             Application.Current.Resources.Add(new DarkTheme());
             DarkMode.IsToggled = true;
+            
+
         }
         else
         {
             mergedDictionaries.Clear();
             Application.Current.Resources.Add(new LightTheme());
             DarkMode.IsToggled = false;
+
+
+
         }
+
         LoadSetting();
+		
+        
         var football = TeamRepository.GetTeams();
 		var cricket = IplRepository.GetTeams();
 		FavoritePicker.ItemsSource = football;
 		FavoriteIplPicker.ItemsSource = cricket;
+
+		
 	}
 	
     private void DarkMode_Toggled(object sender, ToggledEventArgs e)
@@ -40,13 +57,18 @@ public partial class Setting : ContentPage
 			mergedDictionaries.Clear();
 			mergedDictionaries.Add(new DarkTheme());
 			IsDarkMode= true;
+			
+			
 		}
 		else
 		{
 			mergedDictionaries.Clear();
 			mergedDictionaries.Add(new LightTheme());
 			IsDarkMode= false;
+			
+            
 		}
+		
     }
 
 	private void LoadSetting()
@@ -57,6 +79,8 @@ public partial class Setting : ContentPage
     private void FavoritePicker_SelectedIndexChanged(object sender, EventArgs e)
     {
 		_football = FavoritePicker.SelectedItem as List<Team>;
+
+
     }
 
     private void FavoriteIplPicker_SelectedIndexChanged(object sender, EventArgs e)
@@ -75,7 +99,9 @@ public partial class Setting : ContentPage
 		{
             Application.Current.Resources.Add(new LightTheme());
 			IsDarkMode= false;
+
         }
+
     }
     
 
@@ -85,5 +111,6 @@ public partial class Setting : ContentPage
 
         await DisplayAlert("Save", "Changes are saved", "OK");
 		await Navigation.PushAsync(new HomePage(darkMode));
+		
     }
 }
